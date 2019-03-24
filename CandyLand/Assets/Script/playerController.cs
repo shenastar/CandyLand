6434f2b332439de6d8fd11c73cd8380e091e8ee1
shenastar <senanomorsatu@gmail.com>
2019-03-24 13:31:46 +0700

    load assets

---
+++ CandyLand/Assets/Script/playerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    private Animator anim;
+    
+
+   public void Start()
+    {
+        anim = GetComponent<Animator>();
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround); 
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Debug.Log("facing right = false");
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0) {
+            Debug.Log("facing left");
+            Flip();
+        }
+    }
+
+    private void Update()
+    {
+        if (isGrounded == true) {
+            extraJumps = extraJumpsValue;
+            anim.SetBool("isJumping", false);
+
+        }
+      
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            anim.SetTrigger("takeOff");
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true) {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (moveInput == 0)
+        {
+            anim.SetBool("isRunning", false);
+        }
+        else {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isJumping", true);
+                }
+    }
+
+    void Flip() {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
---
+++ CandyLand/Assets/movement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public int playerSpeed, jumpSpeed;
+    public bool facingRight = true, isGround = false;
+    public float moveX;
+    private Animator anim;
+
+    void Update()
+    {
+        walk();
+        jump();
+    }
+
+    void walk()
+    {
+
+        moveX = Input.GetAxis("Horizontal");
+        if (moveX < 0.0f && facingRight == false)
+        {
+            FlipPlayer();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer();
+        }
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void jump()
+    {
+        if (isGround)
+        {
+            if (Input.GetButtonDown("Vertical"))
+            {
+                Debug.Log("jump");
+                gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.GetComponent<Rigidbody2D>().velocity.x, jumpSpeed);
+                isGround = false;
+            }
+        }
+    }
+    void FlipPlayer()
+    {
+        facingRight = !facingRight;
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+}
